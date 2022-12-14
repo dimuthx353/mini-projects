96c64fc266f654ca8e9cff7506117e1d4dccbd27
dimuthx353 <dimuthx353@gmail.com>
2022-12-14 21:04:01 +0530

    second commit

--- project 01/main.js
+++ project 01/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
+    var open = $("#open");
+    var close = $("#close");
+    var lightbox = $(".lightbox");
+    var mobileTools = $(".mobile-tools");
 
+    console.log(mobileTools);
+
+
+
+    open.click(function (e) {
+        lightbox.css({
+            'display': 'flex',
+            'justify-content': 'center',
+            'align-items': 'center'
+        });
+        open.hide();
+        close.css("display", "flex");
+    });
+
+    close.click(function (e) {
+        lightbox.toggle();
+        close.hide();
+        open.show();
+    })
+
+    lightbox.click(function (e) {
+        lightbox.hide();
+        close.hide();
+        open.show();
+    })
 });
